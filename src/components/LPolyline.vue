22e2e17b8c42d3602f450a68c0826a1c3d757234
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2020-10-28 12:17:07 +0100

    feat: add polyline component

---
+++ src/components/LPolyline.vue
@@ -0,0 +1,45 @@
+<script>
+import { onMounted, ref, inject } from "vue";
+import { remapEvents, propsBinder } from "../utils.js";
+import { props, setup as polylineSetup } from "../functions/polyline";
+
+/**
+ * Polyline component, lets you add and personalize polylines on the map
+ */
+export default {
+  name: "LPolyline",
+  props,
+  setup(props, context) {
+    const leafletRef = ref({});
+    const ready = ref(false);
+
+    const addLayer = inject("addLayer");
+
+    const { options, methods } = polylineSetup(props, leafletRef, context);
+
+    onMounted(async () => {
+      const { polyline, DomEvent, setOptions } = await import(
+        "leaflet/dist/leaflet-src.esm"
+      );
+
+      leafletRef.value = polyline(props.latLngs, options);
+
+      const listeners = remapEvents(context.attrs);
+      DomEvent.on(leafletRef.value, listeners);
+
+      propsBinder(methods, leafletRef.value, props, setOptions);
+
+      addLayer({
+        ...props,
+        ...methods,
+        leafletObject: leafletRef.value,
+      });
+      ready.value = true;
+    });
+    return { ready };
+  },
+  render() {
+    return null;
+  },
+};
+</script>
