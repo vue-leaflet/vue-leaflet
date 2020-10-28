a2184d7e26887caf00eae95fd07a81e60fb0f32f
Michael Underwood <mike.underwood@gmail.com>
2020-10-28 16:10:35 -0600

    chore: Add example rectangles to playground

--- src/Playground.vue
+++ src/Playground.vue
@@ -47,6 +47,22 @@
         :fillOpacity="0.5"
         fillColor="#41b782"
       />
+      <l-rectangle
+        :lat-lngs="[
+          [46.334852, -1.509485],
+          [46.342596, -1.328731],
+          [46.241487, -1.190568],
+          [46.234787, -1.358337],
+        ]"
+        :fill="true"
+        color="#35495d"
+      />
+      <l-rectangle
+        :bounds="[
+          [46.334852, -1.190568],
+          [46.241487, -1.090357],
+        ]"
+      />
     </l-map>
     <button @click="changeIcon">New kitten icon</button>
   </div>
@@ -61,6 +77,7 @@ import LTooltip from "./components/LTooltip.vue";
 import LPopup from "./components/LPopup.vue";
 import LPolyline from "./components/LPolyline.vue";
 import LPolygon from "./components/LPolygon.vue";
+import LRectangle from "./components/LRectangle.vue";
 
 export default {
   components: {
@@ -73,6 +90,7 @@ export default {
     LPopup,
     LPolyline,
     LPolygon,
+    LRectangle,
   },
   data() {
     return {
