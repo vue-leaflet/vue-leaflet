8093ba6e782375307948560b938f17c2e7300efe
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2020-10-30 12:17:27 +0100

    chore: wire new components

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,7 @@ export { default as LControlLayers } from "./LControlLayers.vue";
 export { default as LMap } from "./LMap.vue";
 export { default as LMarker } from "./LMarker.vue";
 export { default as LTileLayer } from "./LTileLayer.vue";
+export { default as LIcon } from "./LIcon.vue";
+export { default as LPolyline } from "./LPolyline.vue";
+export { default as LTooltip } from "./LTooltip.vue";
+export { default as LPopup } from "./LPopup.vue";
