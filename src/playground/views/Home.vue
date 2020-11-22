3bebb6b5a20ebd5587d3877e36287d73b78f9b55
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2020-11-22 14:46:51 +0100

    feat: add main component

---
+++ src/playground/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+    <l-tile-layer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      layer-type="base"
+      name="OpenStreetMap"
+    ></l-tile-layer>
+  </l-map>
+</template>
+<script>
+import { LMap, LTileLayer } from "./../../components";
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+  },
+  data() {
+    return {
+      zoom: 2,
+    };
+  },
+};
+</script>
+
+<style></style>
