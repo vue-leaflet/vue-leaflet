aa531a02c82ce4fceb516b35dc57ddf4c46c6d89
Culdred <elia.gentilucci@gmail.com>
2020-11-28 17:50:17 +0100

    Add Circle component

---
+++ src/playground/views/Circle.vue
@@ -0,0 +1,29 @@
+<template>
+  <l-map ref="map" v-model:zoom="zoom" :center="[35.865, 12.865]">
+    <l-tile-layer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      layer-type="base"
+      name="OpenStreetMap"
+    ></l-tile-layer>
+
+    <l-circle :lat-lng="[35.865, 12.865]" :radius="10000" color="green" />
+  </l-map>
+</template>
+<script>
+import { LMap, LTileLayer, LCircle } from "./../../components";
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LCircle,
+  },
+  data() {
+    return {
+      zoom: 8,
+    };
+  },
+};
+</script>
+
+<style></style>
