de192e0afc1070625877f2347962b267c1da1341
Culdred <elia.gentilucci@gmail.com>
2020-11-28 15:36:10 +0100

    Add routing for Scale component

--- src/playground/index.js
+++ src/playground/index.js
@@ -8,6 +8,10 @@ const routes = [
   { path: "/", component: Home },
   { path: "/marker", component: () => import("./views/Marker.vue") },
   { path: "/circle", component: () => import("./views/Circle.vue") },
+  {
+    path: "/control-scale",
+    component: () => import("./views/ControlScale.vue"),
+  },
 ];
 
 const router = createRouter({
