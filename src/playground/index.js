eedec5f84c040ec485c5934f747a01d93bd8d3d8
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2020-11-29 15:29:02 +0100

    chore: add grid-layer to playground

--- src/playground/index.js
+++ src/playground/index.js
@@ -6,6 +6,7 @@ import Home from "./views/Home.vue";
 
 const routes = [
   { path: "/", component: Home },
+  { path: "/grid-layer", component: () => import("./views/GridLayer.vue") },
   { path: "/marker", component: () => import("./views/Marker.vue") },
 ];
 
