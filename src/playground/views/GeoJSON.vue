5c2f3f27d83ab9ae466b24e005d893e07887d6e2
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2020-11-29 18:03:40 +0100

    docs: add geo-json playground

---
+++ src/playground/views/GeoJSON.vue
@@ -0,0 +1,35 @@
+<template>
+  <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+    <l-tile-layer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      layer-type="base"
+      name="OpenStreetMap"
+    ></l-tile-layer>
+    <l-geo-json :geojson="geojson"></l-geo-json>
+  </l-map>
+</template>
+<script>
+import { LMap, LTileLayer, LGeoJson } from "./../../components";
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LGeoJson,
+  },
+  data() {
+    return {
+      zoom: 8,
+      geojson: null,
+    };
+  },
+  async created() {
+    const response = await fetch(
+      "https://rawgit.com/gregoiredavid/france-geojson/master/regions/pays-de-la-loire/communes-pays-de-la-loire.geojson"
+    );
+    this.geojson = await response.json();
+  },
+};
+</script>
+
+<style></style>
